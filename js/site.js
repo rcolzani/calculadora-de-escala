022fac7744722406461442fb659af9812446036c
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2021-07-09 11:25:54 -0300

    Adiciona copyright e melhora estilo de página

--- js/site.js
+++ js/site.js
@@ -1,46 +1,47 @@
 function calcularHorarioFinalDaJornada() {
+    moment.locale('pt-br');
     var inicioJornada = moment('2021-07-08');
     let tempoTotalDeJornada = document.getElementById('tempoTotal').value;
 
-    const intervalo= 60;
+    const intervalo = 60;
 
     let horarioInicial = document.getElementById('horaInicial').value;
     let [horaInicial, minutoInicial] = horarioInicial.split(':');
-    
+
     inicioJornada.add(minutoInicial, 'minutes')
     inicioJornada.add(horaInicial, 'hour')
 
     let diferencaHorario = 0;
     let horarioFinalDaJornada = 0;
 
-    let [horasParaAcrescentar ,minutosParaAcrescentar] = tempoTotalDeJornada.split(':');
+    let [horasParaAcrescentar, minutosParaAcrescentar] = tempoTotalDeJornada.split(':');
 
     let fimJornada = inicioJornada;
     let jornadaReduzia = false;
     let umaHoraEquivaleA = 60;
 
-    while(horasParaAcrescentar > 0 || minutosParaAcrescentar>0){
-        
-        if(!jornadaReduzia){
-            if(fimJornada.hour() >= 22 || fimJornada.hour() < 5){
+    while (horasParaAcrescentar > 0 || minutosParaAcrescentar > 0) {
+
+        if (!jornadaReduzia) {
+            if (fimJornada.hour() >= 22 || fimJornada.hour() < 5) {
                 jornadaReduzia = true;
                 umaHoraEquivaleA = 52.5;
             }
         }
-        
-        if(horasParaAcrescentar > 0){
+
+        if (horasParaAcrescentar > 0) {
             fimJornada.add(umaHoraEquivaleA, 'minutes');
-            horasParaAcrescentar --;
-        }else{
-            if (jornadaReduzia){
-                minutosParaAcrescentar = Number(minutosParaAcrescentar) -(Number(minutosParaAcrescentar) * 0.125);
+            horasParaAcrescentar--;
+        } else {
+            if (jornadaReduzia) {
+                minutosParaAcrescentar = Number(minutosParaAcrescentar) - (Number(minutosParaAcrescentar) * 0.125);
             }
             fimJornada.add(minutosParaAcrescentar, 'minutes');
-            minutosParaAcrescentar =0;
+            minutosParaAcrescentar = 0;
         }
     }
 
-    if(document.getElementById('adicionarIntervalo').checked == true){
+    if (document.getElementById('adicionarIntervalo').checked == true) {
         fimJornada.add(intervalo, 'minutes');
     }
 
